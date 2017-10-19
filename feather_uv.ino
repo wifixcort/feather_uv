0a4136c6dc39e4630f44402e1f52303e51bbc4e9
Ricardo Mena C <rgacort12@gmail.com>
2017-10-18 22:15:28 -0600

    Primera prueva de funcionamiento

--- feather_uv.ino
+++ feather_uv.ino
@@ -9,15 +9,19 @@ void setup() {
 }//end setup
 
 void loop() {
-
+  Serial.println(lecturas_V_sensor_UV());
+  delay(1000);//Esperar un segundo hasta la siguiente medición
 }//end loop
 
-float lecturas_V_sensor_UV(){
+float lecturas_V_sensor_UV(){//Las lecturas deben retornarse en Volts
   /*Este médoto convierte el muestreo realizado por el pin analógico del sensor
     a valores de tensión[V] para así luego poder determinar el indice UV
     Según las especificaciones del sensor la tensión de salida del sensor
     es lineal al indice UV
   */
+  //Se realizará un muestreo de la señal entrante para tener una mejor aproximación
+  float sensor_uv = analogRead(A2);
+  return sensor_uv;
 }//end lecturas_mV_sensor_uv
 
 unsigned int indice_UV(float lectura_V){
